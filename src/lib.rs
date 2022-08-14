04f7110af3e672d2ef3009f06809cedc07856480
jorgeantonio21 <jtiago1993@gmail.com>
2022-08-14 19:45:16 +0100

    add general types

--- src/lib.rs
+++ src/lib.rs
@@ -1,164 +1,278 @@
 use std::cmp::max;
+use std::marker::PhantomData;
+use std::convert::TryInto;
 
-const VERSION: usize = 1; // version 1
-const WORDS: usize = 4; // 4-bytes long, or 32-bit long
-const ROUNDS: usize = 12; // 1 round total
-const BYTES: usize = 16; // Key generation of length 10-bytes
-
-/*
- * This function should return a cipher text for a given key and plaintext
- *
- */
-fn encode(key: Vec<u8>, plaintext: Vec<u8>) -> Result<Vec<u8>, &'static str> {
-    if key.len() != BYTES {
-        return Err("invalid encryption key length");
-    }
-    // get A block
-    let mut a_block = [0u8; 4];
-    a_block.copy_from_slice(&plaintext[..WORDS]);
-    let mut a_from_le_bytes = u32::from_le_bytes(a_block);
-
-    // get B block
-    let mut b_block = [0u8; 4];
-    b_block.copy_from_slice(&plaintext[WORDS..]);
-    let mut b_from_le_bytes = u32::from_le_bytes(b_block);
-
-    // let s table
-    let s_table = generate_block_cipher(key);
-
-	// initialize encryption of blocks A and B
-	a_from_le_bytes = a_from_le_bytes.wrapping_add(s_table[0]);
-	b_from_le_bytes = b_from_le_bytes.wrapping_add(s_table[1]);
-
-    // the algorithm uses ROUNDS iterations, but it starts with a zeroth evaluation first
-    for i in 1..(ROUNDS + 1) {
-        a_from_le_bytes = (a_from_le_bytes ^ b_from_le_bytes)
-            .rotate_left(b_from_le_bytes)
-            .wrapping_add(s_table[2 * i]);
-        b_from_le_bytes = (b_from_le_bytes ^ a_from_le_bytes)
-            .rotate_left(a_from_le_bytes)
-            .wrapping_add(s_table[2 * i + 1]);
-    }
-
-    a_block = a_from_le_bytes.to_le_bytes();
-    b_block = b_from_le_bytes.to_le_bytes();
-
-    let mut ciphertext = Vec::new();
-    ciphertext.extend_from_slice(&a_block);
-    ciphertext.extend_from_slice(&b_block);
-    Ok(ciphertext)
+// represents either one of the types `u16`, `u32` or `u64`
+trait Unsigned16To64 {
+    type Bytes: Clone + Copy + AsRef<[u8]> + AsMut<[u8]>;
+    fn copy_from_slice(plaintext: &[u8], start: usize, end: usize) -> Self::Bytes;
+    fn from_le_bytes(bytes: Self::Bytes) -> Self;
+    fn to_le_bytes(self) -> Self::Bytes;
+    fn wrapping_add(self, other: Self) -> Self;
+    fn wrapping_sub(self, other: Self) -> Self;
+    fn wrapping_mul(self, other: Self) -> Self;
+    fn rotate_left(self, other: Self) -> Self;
+    fn rotate_right(self, other: Self) -> Self;
+    fn xor(self, other: Self) -> Self;
+    fn min() -> Self;
+    fn from_str_radix(val: &str, base: u32) -> Self;
+    fn from_usize(val: usize) -> Self;
+    fn to_usize(&self) -> usize;
 }
 
-/*
- * This function should return a plaintext for a given key and ciphertext
- *
- */
-fn decode(key: Vec<u8>, ciphertext: Vec<u8>) -> Result<Vec<u8>, &'static str> {
-    if key.len() != BYTES {
-        return Err("invalid decryption key length");
-    }
+macro_rules! impl_unsigned_16_to_64 {
+    ($($ty:ty),*) => { $(
+        impl Unsigned16To64 for $ty {
+            type Bytes = [u8; std::mem::size_of::<Self>()];
 
-    // get A block
-    let mut a_block = [0u8; 4];
-    a_block.copy_from_slice(&ciphertext[..WORDS]);
-    let mut a_from_le_bytes = u32::from_le_bytes(a_block);
+            fn copy_from_slice(plaintext: &[u8], start: usize, end: usize) -> Self::Bytes {
+                let mut output = [0u8; std::mem::size_of::<Self>()];
+                output.copy_from_slice(plaintext[start..end].as_ref());
+                output
+            }
 
-    // get B block
-    let mut b_block = [0u8; 4];
-    b_block.copy_from_slice(&ciphertext[WORDS..]);
-    let mut b_from_le_bytes = u32::from_le_bytes(b_block);
+            fn from_le_bytes(bytes: Self::Bytes) -> Self {
+                <$ty>::from_le_bytes(bytes)
+            }
 
-    // get s table
-    let s_table = generate_block_cipher(key);
+            fn to_le_bytes(self) -> Self::Bytes {
+                self.to_le_bytes()
+            }
 
-    // the algorithm uses ROUND iterations, but it starts with a zeroth evaluation first
-    for i in (1..(ROUNDS + 1)).rev() {
-        b_from_le_bytes = b_from_le_bytes
-            .wrapping_sub(s_table[2 * i + 1])
-            .rotate_right(a_from_le_bytes)
-            ^ a_from_le_bytes;
-        a_from_le_bytes = a_from_le_bytes
-            .wrapping_sub(s_table[2 * i])
-            .rotate_right(b_from_le_bytes)
-            ^ b_from_le_bytes;
-    }
+            fn wrapping_add(self, other: Self) -> Self {
+                self.wrapping_add(other)
+            }
 
-    // last iteration
-    a_from_le_bytes = a_from_le_bytes.wrapping_sub(s_table[0]);
-    b_from_le_bytes = b_from_le_bytes.wrapping_sub(s_table[1]);
+            fn wrapping_sub(self, other: Self) -> Self {
+                self.wrapping_sub(other)
+            }
 
-    a_block = a_from_le_bytes.to_le_bytes();
-    b_block = b_from_le_bytes.to_le_bytes();
-	
-    let mut plaintext = Vec::new();
-    plaintext.extend_from_slice(&a_block);
-    plaintext.extend_from_slice(&b_block);
+            fn wrapping_mul(self, other: Self) -> Self {
+                self.wrapping_mul(other)
+            }
 
-    Ok(plaintext)
+            fn rotate_left(self, other: Self) -> Self {
+                self.rotate_left(other.try_into().unwrap())
+            }
+
+            fn rotate_right(self, other: Self) -> Self {
+                self.rotate_right(other.try_into().unwrap())
+            }
+
+            fn xor(self, other: Self) -> Self {
+                self ^ other
+            }
+
+            fn min() -> Self {
+                Self::MIN
+            }
+
+            fn from_str_radix(val: &str, base: u32) -> Self {
+                Self::from_str_radix(val, base).unwrap()
+            }
+
+            fn from_usize(val: usize) -> Self {
+                val as Self
+            }
+
+            fn to_usize(&self) -> usize {
+                *self as usize
+            }
+        }
+    )* }
+}
+impl_unsigned_16_to_64!(u16, u32, u64);
+
+impl CipherMagicConstants for u16 {
+    const P_W: &'static str = "b7e1"; // first magic number
+    const Q_W: &'static str = "9e37"; // second magic number
 }
 
-/*
- * This function should return the key generation for the RC5 algorithm
- *
- */
-fn generate_block_cipher(key: Vec<u8>) -> Vec<u32> {
-    // by the protocol design, we are guaranteed that the length of the
-    // key block is less than 255 = 2^8 - 1
-    let mut l = if key.is_empty() {
-        vec![0u32]
-    } else {
-        (0..(key.len() as u8))
-            .collect::<Vec<u8>>()
+impl CipherMagicConstants for u32 {
+    const P_W: &'static str = "b7e15163"; // first magic number
+    const Q_W: &'static str = "9e3779b9"; // second magic number
+}
+
+impl CipherMagicConstants for u64 {
+    const P_W: &'static str = "b7e151628aed2a6b"; // fist magic number
+    const Q_W: &'static str = "9e3779b97f47c15"; // second magic number
+}
+
+trait Rc5CipherStream<T: Unsigned16To64> {
+    fn encode(&self, plaintext: Vec<u8>) -> Result<Vec<u8>, &'static str>;
+    fn decode(&self, plaintext: Vec<u8>) -> Result<Vec<u8>, &'static str>;
+    fn generate_block_cipher(&self) -> Vec<T>;
+}
+
+trait CipherMagicConstants {
+    const P_W: &'static str;
+    const Q_W: &'static str;
+}
+
+struct RC5<T: Unsigned16To64> {
+    key: Vec<u8>,
+    words: usize,
+    rounds: usize,
+    bytes: usize,
+    data: PhantomData<T>,
+}
+
+impl<T: Unsigned16To64> RC5<T> {
+    fn new(key: Vec<u8>, words: usize, rounds: usize, bytes: usize) -> Self {
+        RC5 {
+            key,
+            words,
+            rounds,
+            bytes,
+            data: PhantomData,
+        }
+    }
+}
+
+impl<T: Unsigned16To64 + CipherMagicConstants + Copy> Rc5CipherStream<T> for RC5<T> {
+    fn encode(&self, plaintext: Vec<u8>) -> Result<Vec<u8>, &'static str> {
+        if self.key.len() != self.bytes {
+            return Err("invalid encryption key length");
+        }
+        // get A block
+        let mut a_block = T::copy_from_slice(&plaintext, 0usize, self.words);
+        let mut a_from_le_bytes = T::from_le_bytes(a_block);
+
+        // get B block
+        let mut b_block = T::copy_from_slice(&plaintext, self.words, plaintext.len());
+        let mut b_from_le_bytes = T::from_le_bytes(b_block);
+
+        // let s table
+        let s_table = self.generate_block_cipher();
+
+        // initialize encryption of blocks A and B
+        a_from_le_bytes = a_from_le_bytes.wrapping_add(s_table[0]);
+        b_from_le_bytes = b_from_le_bytes.wrapping_add(s_table[1]);
+
+        // the algorithm uses ROUNDS iterations, but it starts with a zeroth evaluation first
+        for i in 1..(self.rounds + 1) {
+            a_from_le_bytes = (a_from_le_bytes.xor(b_from_le_bytes))
+                .rotate_left(b_from_le_bytes)
+                .wrapping_add(s_table[2 * i]);
+            b_from_le_bytes = (b_from_le_bytes.xor(a_from_le_bytes))
+                .rotate_left(a_from_le_bytes)
+                .wrapping_add(s_table[2 * i + 1]);
+        }
+
+        a_block = a_from_le_bytes.to_le_bytes();
+        b_block = b_from_le_bytes.to_le_bytes();
+
+        let mut ciphertext = Vec::new();
+        ciphertext.extend_from_slice(a_block.as_ref());
+        ciphertext.extend_from_slice(b_block.as_ref());
+        Ok(ciphertext)
+    }
+
+    fn decode(&self, plaintext: Vec<u8>) -> Result<Vec<u8>, &'static str> {
+        if self.key.len() != self.bytes {
+            return Err("invalid decryption key length");
+        }
+
+        // get A block
+        let mut a_block = T::copy_from_slice(&plaintext, 0usize, self.words);
+        let mut a_from_le_bytes = T::from_le_bytes(a_block);
+
+        // get B block
+        let mut b_block = T::copy_from_slice(&plaintext, self.words, plaintext.len());
+        let mut b_from_le_bytes = T::from_le_bytes(b_block);
+
+        // get s table
+        let s_table = self.generate_block_cipher();
+
+        // the algorithm uses ROUND iterations, but it starts with a zeroth evaluation first
+        for i in (1..(self.rounds + 1)).rev() {
+            b_from_le_bytes = b_from_le_bytes
+                .wrapping_sub(s_table[2 * i + 1])
+                .rotate_right(a_from_le_bytes)
+                .xor(a_from_le_bytes);
+            a_from_le_bytes = a_from_le_bytes
+                .wrapping_sub(s_table[2 * i])
+                .rotate_right(b_from_le_bytes)
+                .xor(b_from_le_bytes);
+        }
+
+        // last iteration
+        a_from_le_bytes = a_from_le_bytes.wrapping_sub(s_table[0]);
+        b_from_le_bytes = b_from_le_bytes.wrapping_sub(s_table[1]);
+
+        a_block = a_from_le_bytes.to_le_bytes();
+        b_block = b_from_le_bytes.to_le_bytes();
+
+        let mut plaintext = Vec::new();
+        plaintext.extend_from_slice(a_block.as_ref());
+        plaintext.extend_from_slice(b_block.as_ref());
+
+        Ok(plaintext)
+    }
+
+    fn generate_block_cipher(&self) -> Vec<T> {
+        // by the protocol design, we are guaranteed that the length of the
+        // key block is less than 255 = 2^8 - 1
+        let mut l = if self.key.is_empty() {
+            vec![T::min()]
+        } else {
+            (0..(self.key.len() as u8))
+                .collect::<Vec<u8>>()
+                .into_iter()
+                .step_by(self.words)
+                .map(|i| {
+                    let slice = T::copy_from_slice(&self.key, i as usize, i as usize + 4);
+                    T::from_le_bytes(slice)
+                })
+                .collect::<Vec<T>>()
+        };
+
+        let p_w = T::from_str_radix(T::P_W, 16); // first magic number
+        let q_w = T::from_str_radix(T::Q_W, 16); // second magic number
+
+        let s_table = 0..(2 * (self.rounds + 1));
+        let mut s_table = s_table
             .into_iter()
-            .step_by(WORDS)
-            .map(|i| {
-                let mut slice = [0u8; WORDS];
-                slice.copy_from_slice(&key[(i as usize)..(i as usize + 4)]);
-                u32::from_le_bytes(slice)
-            })
-            .collect::<Vec<u32>>()
-    };
+            .map(|x| {
+                T::from_usize(x).wrapping_mul(q_w).wrapping_add(p_w)})
+            .collect::<Vec<T>>();
 
-    let p_w = u32::from_str_radix("b7e15163", 16).unwrap(); // first magic number
-    let q_w = u32::from_str_radix("9e3779b9", 16).unwrap(); // second magic number
+        let mut i = T::min();
+        let mut j = T::min();
 
-    let s_table = 0..(2u32 * (ROUNDS as u32 + 1));
-    let mut s_table = s_table
-        .into_iter()
-        .map(|x| x.wrapping_mul(q_w).wrapping_add(p_w))
-        .collect::<Vec<u32>>();
+        let mut a_block = T::min();
+        let mut b_block = T::min();
 
-    let mut i = 0u32;
-    let mut j = 0u32;
+        let l_len = l.len();
+        let s_len = s_table.len();
 
-    let mut a_block = 0u32;
-    let mut b_block = 0u32;
+        let max_iters = max(s_len, l_len);
 
-    let l_len = l.len() as u32;
-    let s_len = s_table.len() as u32;
+        for _ in 0..(3 * max_iters) {
+            a_block = s_table[i.to_usize()]
+                .wrapping_add(a_block)
+                .wrapping_add(b_block)
+                .rotate_left(T::from_usize(3usize));
+            b_block = (l[j.to_usize()].wrapping_add(a_block).wrapping_add(b_block))
+                .rotate_left(a_block.wrapping_add(b_block));
 
-    let max_iters = max(s_len, l_len);
-    for _ in 0..(3 * max_iters) {
-        a_block = s_table[i as usize]
-            .wrapping_add(a_block)
-            .wrapping_add(b_block)
-            .rotate_left(3u32);
-        b_block = (l[j as usize].wrapping_add(a_block).wrapping_add(b_block))
-            .rotate_left(a_block.wrapping_add(b_block));
+            s_table[i.to_usize()] = a_block;
+            l[j.to_usize()] = b_block;
 
-        s_table[i as usize] = a_block;
-        l[j as usize] = b_block;
+            i = T::from_usize((i.to_usize() + 1) % s_len);
+            j = T::from_usize((j.to_usize() + 1) % l_len);
+        }
 
-        i = (i + 1) % s_len;
-        j = (j + 1) % l_len;
+        s_table
     }
-
-    s_table
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const WORDS: usize = 4; // 4-bytes long, or 32-bit long
+    const ROUNDS: usize = 12; // 1 round total
+    const BYTES: usize = 16; // Key generation of length 10-bytes
 
     #[test]
     fn encode_a() {
@@ -166,9 +280,12 @@ mod tests {
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
             0x0E, 0x0F,
         ];
+
+        let rc_5 = RC5::<u32>::new(key, WORDS, ROUNDS, BYTES);
+
         let pt = vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77];
         let ct = vec![0x2D, 0xDC, 0x14, 0x9B, 0xCF, 0x08, 0x8B, 0x9E];
-        let res = encode(key, pt).unwrap();
+        let res = rc_5.encode(pt).unwrap();
         assert_eq!(ct, res);
     }
 
@@ -178,9 +295,12 @@ mod tests {
             0x2B, 0xD6, 0x45, 0x9F, 0x82, 0xC5, 0xB3, 0x00, 0x95, 0x2C, 0x49, 0x10, 0x48, 0x81,
             0xFF, 0x48,
         ];
+
+        let rc_5 = RC5::<u32>::new(key, WORDS, ROUNDS, BYTES);
+
         let pt = vec![0xEA, 0x02, 0x47, 0x14, 0xAD, 0x5C, 0x4D, 0x84];
         let ct = vec![0x11, 0xE4, 0x3B, 0x86, 0xD2, 0x31, 0xEA, 0x64];
-        let res = encode(key, pt).unwrap();
+        let res = rc_5.encode(pt).unwrap();
         assert_eq!(ct, res);
     }
 
@@ -190,9 +310,12 @@ mod tests {
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
             0x0E, 0x0F,
         ];
+
+        let rc_5 = RC5::<u32>::new(key, WORDS, ROUNDS, BYTES);
+
         let pt = vec![0x96, 0x95, 0x0D, 0xDA, 0x65, 0x4A, 0x3D, 0x62];
         let ct = vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77];
-        let res = decode(key, ct).unwrap();
+        let res = rc_5.decode(ct).unwrap();
         assert!(&pt[..] == &res[..]);
     }
 
@@ -202,9 +325,12 @@ mod tests {
             0x2B, 0xD6, 0x45, 0x9F, 0x82, 0xC5, 0xB3, 0x00, 0x95, 0x2C, 0x49, 0x10, 0x48, 0x81,
             0xFF, 0x48,
         ];
+
+        let rc_5 = RC5::<u32>::new(key, WORDS, ROUNDS, BYTES);
+
         let pt = vec![0x63, 0x8B, 0x3A, 0x5E, 0xF7, 0x2B, 0x66, 0x3F];
         let ct = vec![0xEA, 0x02, 0x47, 0x14, 0xAD, 0x5C, 0x4D, 0x84];
-        let res = decode(key, ct).unwrap();
+        let res = rc_5.decode(ct).unwrap();
         assert!(&pt[..] == &res[..]);
     }
 }
